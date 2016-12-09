d007c7065655c046fb7972ac169a7633577cda39
kaizsv <kai.zsv@gmail.com>
2016-12-10 04:43:47 +0800

    continue

--- board.py
+++ board.py
@@ -1,12 +1,12 @@
 import numpy as np
+import math
 
 class Board:
     def __init__(self, n):
         self.size = n
         self.env = np.zeros((self.size, self.size))
-        self.legal_moves = [i for i in range(1, self.size ** 2 + 1)]
+        self.legal_moves = [i for i in range(self.size ** 2)]
         self.symbol = {0:'-', 1:'X', 2:'O'}
-        self.model = dict()
 
     def __str__(self):
         # return a string that print current environment
@@ -25,3 +25,29 @@ class Board:
         s += space + u'= ' * self.size
         return s
 
+    def reset(self):
+        self.env = np.zeros((self.size, self.size))
+        self.legal_moves = [i for i in range(self.size ** 2)]
+
+    def is_legal_move(self, action):
+        return action in legal_moves
+
+    def decode_action(self, action):
+        return math.ceil(action / self.size), (action % self.size)
+
+    def set_action(self, action, symbol):
+        pos_x, pos_y = self.decode_action(action)
+        self.env[pos_x][pos_y] = symbol
+        self.legal_moves.remove(action)
+
+    def fair_board(self):
+        # black can olny move outer lines of the board
+        # at the first move.
+        limit_line = 2
+        while True:
+            first_move = np.random.choice(self.legal_moves, 1)
+            if first_move < self.size * limit_line or \
+                first_move > self.size**2 - 1 - self.size * limit_line or \
+                first_move % self.size < limit_line or \
+                first_move % self.size > self.size - 1 - limit_line:
+                return first_move
--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import numpy as np
 from player import Player
 from agent import Agent
 from board import Board
@@ -11,7 +12,7 @@ class Game:
         self.game_condition()
 
     def game_condition(self):
-        condition = input('\nChoose game mode\n1) single play\n2) learning\n>')
+        condition = input('\nChoose game mode\n1) single player\n2) learning\n>')
         if condition == 1:
             first = input('\nYou want to play\n1) black\n2) white\n>')
             if first == 1:
@@ -23,15 +24,29 @@ class Game:
         elif condition == 2:
             self.player1 = Agent(1, True)
             self.player2 = Agent(2, True)
+            # initial weights
+            W_size = self.board.size ** 2
+            self.W = np.random.rand(W_size, W_size)
+            self.W = self.W / (2 * W_size)
 
+            # start learning
             self.reinforcement_learning()
         else:
             # wrong input, choose again
             return self.game_condition()
 
-
     def get_players(self):
         return self.player1, self.player2
 
     def reinforcement_learning(self):
-        print(self.board)
+        iter = 100
+        max_seq = self.board.size ** 2
+        for j in range(iter):
+            self.board.reset()
+            start_state = self.board.fair_board()
+            reward = 0
+            states_seq = []
+            action_probability_seq = []
+            action_seq = []
+            #for i in range(max_seq):
+                
